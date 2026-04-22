c2d1311be9cfdf24917cb26a782eca7f20d832f2
Anna Wurtz <annawurtz123@gmail.com>
2026-04-22 16:33:39 -0500

    Add clicking logic for adding symbols to the grid

--- src/Grid.java
+++ src/Grid.java
@@ -1,13 +1,14 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.Rectangle;
 
 public class Grid extends GraphicsGroup {
+    public static final int GRID_WIDTH = 420;
+    public static final int GRID_HEIGHT = 420;
     private Rectangle board;
-    private List<Rectangle> cells;
-    private static final int GRID_WIDTH = 420;
-    private static final int GRID_HEIGHT = 420;
+    public List<Rectangle> cells = new ArrayList();
     private int x = 90;
     private int xnew = x;
     private int y = 190;
@@ -17,14 +18,19 @@ public class Grid extends GraphicsGroup {
     public Grid() {
         board = new Rectangle(x,y,GRID_WIDTH,GRID_HEIGHT);
         add(board);
-        for (int i=0;i<8;i++) {
+        for (int i=0;i<9;i++) {
             if (xnew + GRID_WIDTH/3 > x + GRID_WIDTH) {
                 xnew = x;
                 y += GRID_HEIGHT/3;
             }
             Rectangle cell = new Rectangle(xnew,y,GRID_WIDTH/3,GRID_HEIGHT/3);
             add(cell);
+            cells.add(cell);
             xnew += GRID_WIDTH/3;
         }
+    }
+
+    public List<Rectangle> getCells() {
+        return cells;
     }
 }
--- src/TicTacToeManager.java
+++ src/TicTacToeManager.java
@@ -1,5 +1,8 @@
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsGroup;
+import edu.macalester.graphics.GraphicsObject;
 import edu.macalester.graphics.Image;
+import edu.macalester.graphics.Rectangle;
 
 public class TicTacToeManager {
     private static final int CANVAS_WIDTH = 600;
@@ -10,19 +13,24 @@ public class TicTacToeManager {
 
     public TicTacToeManager() {
         canvas = new CanvasWindow("Tic-Tac-Toe!", CANVAS_WIDTH, CANVAS_HEIGHT);
-        // Image exSymbol = new Image(0, 0, "ex.png");
-        // exSymbol.setScale(0.5);
-        // canvas.add(exSymbol);
-        // Image ohSymbol = new Image(300, 300, "oh.png");
-        // ohSymbol.setScale(0.5);
-        // canvas.add(ohSymbol);
         grid = new Grid();
         canvas.add(grid);
         canvas.onMouseDown(event -> {
-            Image exSymbol = new Image(0, 0, "ex.png");
-            canvas.add(exSymbol);
-            exSymbol.setPosition(event.getPosition());
-            exSymbol.setScale(0.5);
+            double x = event.getPosition().getX();
+            double y = event.getPosition().getY();
+
+            GraphicsObject clicked = grid.getElementAtLocalCoordinates(x, y);
+
+            if (clicked != null) {
+                Image exSymbol = new Image(x, y, "ex.png");
+                double paddingX = (clicked.getWidth() - exSymbol.getWidth()/2)/2;
+                double paddingY = (clicked.getHeight() - exSymbol.getHeight()/2)/2;
+                double xnew = clicked.getPosition().getX() - exSymbol.getWidth()/4 + paddingX;
+                double ynew = clicked.getPosition().getY() - exSymbol.getHeight()/4 + paddingY;
+                exSymbol.setScale(0.5);
+                exSymbol.setPosition(xnew,ynew);
+                canvas.add(exSymbol);
+            }
         });
     }
     public static void main(String[] args){
