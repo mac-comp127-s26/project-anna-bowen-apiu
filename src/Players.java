e0cd1d81c11e98e217183411c34959170558bd80
ApiuKur <akur@macalester.edu>
2026-04-24 08:46:40 -0500

    created the players class

---
+++ src/Players.java
@@ -0,0 +1,17 @@
+public class Players {
+    private String name;
+    private String Symbol;
+
+    public Players(String name,String Symbol){
+        this.name=name;
+        this.Symbol=Symbol;
+
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSymbol() {
+        return Symbol;
+    }
+    
+}
